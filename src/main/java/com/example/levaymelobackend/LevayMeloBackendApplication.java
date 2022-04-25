1f3466024007e3cbc7666f13601810820ac9d005
Barnabas Kalydy <barnabas.kalydy.ext@evosoft.com>
2022-04-25 16:09:16 +0200

    initial commit

---
+++ src/main/java/com/example/levaymelobackend/LevayMeloBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.levaymelobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevayMeloBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevayMeloBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/levaymelobackend/LevayMeloBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.levaymelobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevayMeloBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
