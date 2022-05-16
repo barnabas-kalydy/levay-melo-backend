3eded98da49ac784b346f1001e4229e646fcdac7
Barnabas Kalydy <barnabas.kalydy.ext@evosoft.com>
2022-05-16 16:23:07 +0200

    orai munka

--- src/main/java/com/example/levaymelobackend/controller/UserController.java
+++ src/main/java/com/example/levaymelobackend/controller/UserController.java
@@ -5,13 +5,11 @@ import com.example.levaymelobackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
@@ -22,13 +20,18 @@ public class UserController {
 
     @RequestMapping()
     public ResponseEntity<List<UserModel>> getUsers() {
-
         return ResponseEntity.ok(userService.getAllUser());
     }
 
     @RequestMapping(method = POST, path = "/add")
     public ResponseEntity<Void> addUser(@RequestBody UserModel userModel){
         userService.addUser(userModel);
+        return ResponseEntity.ok().build();
+    }
+
+    @RequestMapping(method = DELETE, path = "/delete/{userName}")
+    public ResponseEntity<Void> deleteUser(@PathVariable(name = "userName") String userName) {
+        userService.deleteUser(userName);
         return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/example/levaymelobackend/service/UserService.java
+++ src/main/java/com/example/levaymelobackend/service/UserService.java
@@ -1,16 +1,18 @@
 package com.example.levaymelobackend.service;
 
 import com.example.levaymelobackend.model.UserModel;
+import org.apache.catalina.User;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 @Service
 public class UserService {
 
-    ArrayList<UserModel>users = new ArrayList<>();
+    ArrayList<UserModel> users = new ArrayList<>();
 
     public List<UserModel> getAllUser() {
 
@@ -19,5 +21,15 @@ public class UserService {
 
     public void addUser(UserModel userModel) {
         users.add(userModel);
+    }
+
+    public void deleteUser(String userName) {
+        Iterator<UserModel> i = users.iterator();
+        while (i.hasNext()) {
+            UserModel userModel = i.next();
+            if (userModel.getUsername().equals(userName)) {
+                i.remove();
+            }
+        }
     }
 }
